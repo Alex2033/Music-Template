19903a7133709ad150ee72371ab8f015ca335f06
Alexandr <XoxlofAlexandr@yandex.ru>
2019-01-16 01:53:30 +0300

    1. Добавлен слайдер для секции брендов 2. Добавлена секция новостей

--- build/js/main.js
+++ build/js/main.js
@@ -10,6 +10,10 @@ $(document).on('click', '.scrollDown__btn', function(){
     fullpage_api.moveSectionDown();
 });
 
+$(document).on('click', '.scrollUp__btn', function(){
+    fullpage_api.moveSectionUp();
+});
+
 $(function() {
     $('.toggle').click(function() {
         $(this).toggleClass('active');
@@ -20,5 +24,16 @@ $(function() {
 $(function() {
     $('.released-album__exit').click(function() {
         $('.released-album').remove();
+    });
+});
+
+$(document).ready(function() {
+    $('.js-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev slick-arrow'>Previous <br> Artist</button>",
+        nextArrow:"<button type='button' class='slick-next slick-arrow'>Next <br> Artist</button>"
     });
 });
